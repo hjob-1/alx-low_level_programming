b50d2dfbd5061dfe102fa8da3953d63cbcf4448f
eyob <hjob12tadele@gmail.com>
2022-04-12 12:40:59 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,16 +5,14 @@ void times_table(void)
 {
 	int i, j;
 
-	i = j = 0;
-
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar((i * j) + ',');
+			_putchar(i * j);
+			_putchar(',');
 			_putchar('\t');
 		}
 		_putchar('\n');
-
 	}
 }
