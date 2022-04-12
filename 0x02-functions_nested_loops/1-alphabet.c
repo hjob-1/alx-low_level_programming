10dae70177529ead444e85cfcc814cf8056e84ec
eyob <hjob12tadele@gmail.com>
2022-04-12 06:13:08 -0700

    second commit after fixing coding style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *print_alphabet: prints the alphabet, in lowercase, followed by a new line.
+ *description:print_alphabet  prints the alphabet, in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
 	char ch;
 
-	for (ch = 'a';ch <= 'z';ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
