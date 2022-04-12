86cc8b0a3b094bb24010d2f0607082efd1b097bb
eyob <hjob12tadele@gmail.com>
2022-04-12 05:29:14 -0700

    final commit for putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,10 @@
 #include "main.h"
 
 /**
- *
- *  * main - Entry point.
- *
- *   * Description: write a program that prints "_putchar" followed by a new line.
- *
- *    * Return: Always 0 (Success.)
- *
- *     */
+  * main - Entry point.
+  * Description: write a program that prints "_putchar" followed by a new line.
+  * Return: Always 0 (Success.)
+  */
 
 int main(void)
 {
