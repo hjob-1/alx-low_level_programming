29348d783964be59b6e88497801a21b7b14bc7e9
eyob <hjob12tadele@gmail.com>
2022-04-12 14:08:00 -0700

    11pro

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,8 +10,9 @@ void print_to_98(int n)
 	int i;
 	const int LIMIT = 98;
 
-	if (n > LIMIT)
-	{
+	if (n == LIMIT)
+		printf("%d",n);
+	else if (n > LIMIT) {
 		for (i = n; i >= LIMIT; i--)
 		{
 			printf("%d",i);
