d931875343299dfe94fdacc3f888a87aa192d158
eyob <hjob12tadele@gmail.com>
2022-04-13 12:17:01 -0700

    first commiy

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests functions that prints if integer is postive or negative
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i=98;
+	positive_or_negative(i);
+	
+	return (0);
+}
