6e145e6c0a805ce09454783410702b8e07a2da24
eyob <hjob12tadele@gmail.com>
2022-04-14 05:08:55 -0700

    first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,7 +13,8 @@ int main(void)
 
 	i = 0;
 	/*
-	 * while (i < 10) since i never incremeted never end
+	 * Description - tsince i never incremeneted the loop never end
+	 * while (i < 10)
 	 * {
 	 *	putchar(i);
 	 * }
