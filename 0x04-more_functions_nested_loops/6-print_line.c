705be37739a217af7867ff5e460e63b82cf0b5a1
eyob <hjob12tadele@gmail.com>
2022-04-14 14:35:20 -0700

    task6 commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,13 +11,13 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-	_putchar('\n' + '0');
+		_putchar('\n');
 	}
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar('_' + '0');
+			_putchar('_');
 		}
 	}
 }
