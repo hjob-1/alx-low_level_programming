ca3cd72b20a2a70214a728c1f34effb63e689fef
eyob <hjob12tadele@gmail.com>
2022-04-18 11:42:42 -0700

    task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *_strlen - calculates the length of the string
- *@s : a givem string
- *Return :string length
+ * _strlen - calculates the length of the string
+ * @s: a givem string
+ * Return: string length
  */
 
 int _strlen(char *s)
