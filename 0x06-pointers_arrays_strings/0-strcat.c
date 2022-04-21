351b8a8faa5a2ea6d0a5e17159398b02d0208465
eyob <hjob12tadele@gmail.com>
2022-04-21 01:41:15 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *_strcat : it concatenates two string
+ *
+ * @dest : stores the concatenated strings
+ *
+ * @src : the string append on the dest
+ *
+ * Return: return a character
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int end_of_dest = 0;
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+		end_of_dest++;
+	}
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[end_of_dest] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest); 
+}
