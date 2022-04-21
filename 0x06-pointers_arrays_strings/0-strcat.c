851e9f99b3170a32df700e0e3e4c390b21d74c34
eyob <66901670+hjob-1@users.noreply.github.com>
2022-04-21 04:22:41 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,43 @@
 #include "main.h"
 
 /**
- *_strcat : it concatenates two string
- *
- * @dest : stores the concatenated strings
- *
- * @src : the string append on the dest
- *
- * Return: return a character
+ * _strcat - concatenates two strings
+ * @dest: a pointer to a string
+ * @src: a pointer to a sting
+ * Return: returns a pointer to the resulting string dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int end_of_dest = 0;
-	int i = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-		end_of_dest++;
-	}
-	i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[end_of_dest + i] = src[i];
-		i++;
-	}
-	
-
-	return (dest); 
+int i = 0, j = 0, k = 0;
+int sizedest;
+int sizesrc;
+int totalsize;
+while (i >= 0)
+{
+if (dest[i] == '\0')
+{
+sizedest = i;
+break;
+}
+i++;
+}
+i = 0;
+while (i >= 0)
+{
+if (src[i] == '\0')
+{
+sizesrc = i;
+break;
+}
+i++;
+}
+totalsize = sizedest + sizesrc;
+j = sizedest;
+while (j < totalsize)
+{
+dest[j] = src[k];
+j++;
+k++;
+}
+return (dest);
 }
