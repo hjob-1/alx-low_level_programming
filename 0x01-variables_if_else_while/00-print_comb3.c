15750294b7ca4fc34e1f6f8798e308ead2b35b1b
eyob <66901670+hjob-1@users.noreply.github.com>
2022-04-23 08:17:59 -0700

    Create 00-print_comb3.c

---
+++ 0x01-variables_if_else_while/00-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 89)
+	{
+		if ((i / 10) < (i % 10))
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			if (i != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
