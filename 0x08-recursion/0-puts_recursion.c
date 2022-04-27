8096b23da41fec3dc03d9ad5559931c16b871ea1
eyob <66901670+hjob-1@users.noreply.github.com>
2022-04-27 13:01:05 -0700

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print the strings
- * @s - store the string 
- * Return - returns nothing
+ *_puts_recursion - print the strings
+ *@s: - store the string 
+ *Return: returns nothing
  */
 
 void _puts_recursion(char *s)
