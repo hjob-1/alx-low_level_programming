711c3881f5f7d55b733e7e9d378709a98889be98
eyob <66901670+hjob-1@users.noreply.github.com>
2022-04-27 12:47:45 -0700

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,16 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-
-	if (s[i] != '\0')
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
 	{
 		_putchar('\n');
-		return ;
 	}
-
-	_putchar(s[i]);
-	_puts_recursion(*(s + 1));
 }
