af216663176109122a2ddb00c9320da69bf05358
eyob <hjob12tadele@gmail.com>
2022-04-30 03:33:39 -0700

    task 4 first commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of two numbers followed by new line
+ * @argc: takes in an integer
+ * @argv: takes in a string
+ * Return:returns 1 if it has error and return 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", mul);
+	}
+	return (0);
+}
