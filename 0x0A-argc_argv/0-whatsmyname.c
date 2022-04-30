3e3d2ca444044dd0538bc6ea123b0e1e1dcd56b4
eyob <66901670+hjob-1@users.noreply.github.com>
2022-04-30 02:36:46 -0700

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - prints the name of the program, followed by a new line
  * @argc: takes in an integer
  * @argv: takes in a string
  * Return: Always 0.
  */
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n",argv[0]);
