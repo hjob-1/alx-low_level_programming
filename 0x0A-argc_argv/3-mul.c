6ebe293e2a290fa669d45a1a5745218f916814c2
eyob <66901670+hjob-1@users.noreply.github.com>
2022-04-30 04:31:23 -0700

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  * main - prints the multiplication of two numbers followed by new line
  * @argc: takes in an integer
  * @argv: takes in a string
- * Return:returns 1 if it has error and return 0 if success
+ * Return: always returns 1 if it has error and return 0 if success
  */
 
 int main(int argc, char *argv[])
