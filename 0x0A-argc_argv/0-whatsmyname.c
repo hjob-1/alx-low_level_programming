b8a736a6c079c84c709799dd73ebf32fe9abb0ba
eyob <hjob12tadele@gmail.com>
2022-04-30 02:02:08 -0700

    task 0 first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - display the name of the program.
+ * @argc:  number of arguments
+ * @argv: an array of argc size containing the values 
+ * arguments
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("The name of the program is =%s\n",argv[0]);
+
+	return  (0);
+}
+
