d6054e875b19976e5d5e3a9cfb88780d962719eb
eyob <hjob12tadele@gmail.com>
2022-04-30 02:56:51 -0700

    tak1 first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - display the number or argument
+ *@argc: argument numbers
+ *@argv: an array of argc values
+ *Return: return 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n",argc);
+
+	return (0);
+}
