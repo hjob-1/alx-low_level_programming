eec174a2e695f7ed8c44965310063d7df020968f
eyob <hjob12tadele@gmail.com>
2022-05-02 01:18:01 -0700

    first commit of malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "mian.h"
+#include <stdlib>
+
+/**
+ * create_array - creates an array of chars and initilizes it with a specific char
+ * @size: size of the the array
+ * @c: takes char value
+ * Return: null if size = 0, return pointer to the array or null if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		arr = malloc(size);
+		
+		if ( arr == NULL)
+			return (NULL);
+		else
+		{
+			for (i = 0; i < size; i++)
+			{
+				arr[i] = c;	
+			}
+		return (arr);	
+		}
+	}
+}
