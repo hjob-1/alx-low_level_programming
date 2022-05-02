32c1b57aad330ed1c27eeb0b2da7f39aaa394f50
eyob <hjob12tadele@gmail.com>
2022-05-02 05:25:06 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,9 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)
-			arr[i][j] = 0;
+		{
+			*(arr + i) + j = 0;
+		}
 	}
 	return (arr);
 }
