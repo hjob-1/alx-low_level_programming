b3905be9b0bcae80a0bf7d85761c59c8e2fe8e74
eyob <66901670+hjob-1@users.noreply.github.com>
2022-05-03 06:57:10 -0700

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,34 +1,45 @@
+
 #include "main.h"
 #include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of int
- * @width: takes in integer
- * @height: takes in integer
- * Return: return null in failure,0,or negative valuen else pointer
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: takes in an integer
+ * @height: takes in an integer
+ * Return: a pointer to a 2 dimensional array of integers,
+ *         other wise NULL on failure
  */
 
 int **alloc_grid(int width, int height)
 {
-	int **arr;
-	int i, j, INT_SIZE;
-	INT_SIZE = 4;
-	
+	int wIndex = 0, hIndex = 0, **arr;
+
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	arr = (int**)malloc(INT_SIZE * width * 2);
-
-	for (i = 0; i < width; i++)
-		arr[i] = (int*)malloc(INT_SIZE * height);
-	if (arr == NULL)
-		return (NULL);
-	for (i = 0; i < width; i++)
+	else
 	{
-		for (j = 0; j < height; j++)
+		arr = (int **)malloc(height * sizeof(int *));
+		if (arr == NULL)
+			return (NULL);
+		for (hIndex = 0; hIndex < height; hIndex++)
 		{
-			arr[i][j] = 0;
+			arr[hIndex] = malloc(width * sizeof(int));
+			if (arr[hIndex] == NULL)
+			{
+				for (; hIndex >= 0; hIndex--)
+					free(arr[hIndex]);
+				free(arr);
+				return (NULL);
+			}
+		}
+		for (hIndex = 0; hIndex < height; hIndex++)
+		{
+			for (wIndex = 0; wIndex < width; wIndex++)
+			{
+				arr[hIndex][wIndex] = 0;
+			}
 		}
 	}
 	return (arr);
