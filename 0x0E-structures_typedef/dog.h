3aec307ba5dd9232144e5d44bf68895424f3c580
eyob <hjob12tadele@gmail.com>
2022-05-09 02:21:34 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+
+/**
+ * struct dog - dog attributes
+ * @name: name of the dog instance
+ * @age: takes a real  number
+ * @owner: takes a string value
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+}
