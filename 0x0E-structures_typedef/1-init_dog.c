184c5deae5abc94d94e314899b8760fa56c295cc
eyob <66901670+hjob-1@users.noreply.github.com>
2022-05-09 13:07:23 -0700

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,20 +2,20 @@
 #include "dog.h"
 
 /**
- * init_dog - initilize a dogs attributes
- * @name: takes a string value
- * @age: takes an int value
- * @owner: takes a string value
- * @d: takes an struct 
- * Return: return nothing
+ * init_dog - Initializes a variable of type struct dog
+ * @d: The dog object to be initialized
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the owner of the dog
+ * Return: It does not return anything, it swts the members with input data
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
