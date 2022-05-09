c3323c7667756d033b4169e20be4307c0aec7e2e
eyob <66901670+hjob-1@users.noreply.github.com>
2022-05-09 12:57:02 -0700

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -13,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-	d->*name = name;
+	d->name = name;
 	d->age = age;
-	d->*owner = owner;
+	d->owner = owner;
 	}
 }
