fa8666fc83985d48d4b91d8d3e1383ce82280b2f
eyob <66901670+hjob-1@users.noreply.github.com>
2022-05-09 13:45:07 -0700

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define DOG
 
 /**
+ * dog - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - structure for a dog object
  * @name: name member
  * @age: age memeber
@@ -9,12 +14,14 @@
  * Description: structure called "dog" that stores the name, age and
  *              the name of its owner
  */
-
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /*#ifndef DOG*/
