29aba5bc26f72919cf0640275751f42acfd3610b
eyob <hjob12tadele@gmail.com>
2022-05-13 01:13:51 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,14 +13,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int isfound;
 
-	if (size <= 0 || array || cmp)
+	if (size <= 0)
 		return (-1);
 	
 	for (i = 0; i < size; i++)
 	{
-		isfound = cpm(array[i]);
+		isfound = cmp(array[i]);
 
-		if(isfound)
+		if (isfound)
 			return (i);	
 	}
 	return (-1);
