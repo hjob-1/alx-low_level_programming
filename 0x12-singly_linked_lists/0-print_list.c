574beed140a9c858f4209df5713df6e7b14df288
hjob-1 <hjob12tadele@gmail.com>
2022-05-19 10:51:45 -0700

    fixing the bugs

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,16 +13,12 @@ size_t print_list(const list_t *h)
 {
     size_t len = 0;
 
-    if (h == NULL)
-        return (1);
-
     while (h != NULL)
     {
         if (h->str == NULL)
             printf("[0] (nil) \n");
         else
             printf("[%d] %s \n", h->len, h->str);
-
         len++;
         h = h->next;
     }
