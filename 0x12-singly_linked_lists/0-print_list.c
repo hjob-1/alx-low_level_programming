bab32a37808fadb22a9cdbbc255d0cc9294ae3d3
hjob-1 <hjob12tadele@gmail.com>
2022-05-19 10:42:02 -0700

    print list task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+    int len = 0;
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil) \n");
+        else
+            printf("[%d] %s \n", h->len, h->str);
+
+        len++;
+        h = h->next;
+    }
+    return (len);
+}
