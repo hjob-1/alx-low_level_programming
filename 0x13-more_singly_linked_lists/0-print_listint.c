92e1d2924533643b0fa804ac21481e701bdd1023
eyob <66901670+hjob-1@users.noreply.github.com>
2022-05-25 00:02:36 -0700

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t len = 0;
-    
-    while (h)
-    {
-        printf("%d \n", h->n);
-        len++;
-        h = h->next;
-    }
-    return (len);
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
