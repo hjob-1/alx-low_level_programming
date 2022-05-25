848b179bd87f946ff43f167854d299f3b9679aa5
eyob <66901670+hjob-1@users.noreply.github.com>
2022-05-25 12:47:48 -0700

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,23 +1,20 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- * delete_nodeint_at_index - deletes the node at index index of a
- *                           listint_t linked list
- * @head: pointer to the first node in the list
- * @index:  is the index of the node that should be deleted. Index starts at 0
+ * delete_nodeint_at_index - is a function that delets a node in list
+ * @head: is the pointer to the start of the list
+ * @index: is the index of the node to be deleted
  * Return: 1 if it succeeded, -1 if it failed
  */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *nextNode, *temp;
-	unsigned int nodeCount;
+	listint_t *temp, *tempnext;
+	unsigned int i = 0;
 
 	temp = *head;
-	nodeCount = 0;
-
 	if (*head == NULL)
 		return (-1);
+
 	if (index == 0)
 	{
 		*head = temp->next;
@@ -25,17 +22,17 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (temp != NULL && nodeCount < index - 1)
+	while (temp != NULL && i < index - 1)
 	{
 		temp = temp->next;
-		nodeCount++;
+		i++;
+
 		if (temp == NULL || temp->next == NULL)
 			return (-1);
 	}
 
-	nextNode = temp->next->next;
+	tempnext = temp->next->next;
 	free(temp->next);
-	temp->next = nextNode;
-
-	return (1);
+	temp->next = tempnext;
+	return (-1);
 }
